c4a8be26dadc373df254385fc46e480ea7dff976
iamharsshit <harshit.bansalec@gmail.com>
2017-07-20 19:29:34 +0530

    add cab and allow opeartions on bulk messages by senders

---
+++ app/src/main/java/com/example/android/easymail/interfaces/MailClassifierSenderLongClickListener.java
@@ -0,0 +1,10 @@
+package com.example.android.easymail.interfaces;
+
+/**
+ * Created by Harshit Bansal on 7/19/2017.
+ */
+
+public interface MailClassifierSenderLongClickListener {
+
+    void onSenderLongClicked(int position, String sender);
+}
